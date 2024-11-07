97939a592cbbb57831e64c295e59b664fa3ad443
Moonshallow5 <ssandeepssingh55@gmail.com>
2024-11-07 15:30:33 +0800

    updating weather implementation

--- Weather-app/webpack.config.js
+++ Weather-app/webpack.config.js
@@ -2,22 +2,24 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js", // No hash for consistency in dev mode
+    filename: "main.js", // Keeps filename consistent in development
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+    clean: true, // Clears old files in dist before each build
   },
-  mode: "development",
   devtool: "eval-source-map",
   devServer: {
-    static: "./dist",
+    static: path.resolve(__dirname, "dist"), // Serve from dist folder
     port: 8080,
     watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: "./src/template.html", // Correct template path
+      filename: "index.html", // Ensures only one index.html is generated
+      inject: "body", // Ensures script is injected at the end of body
     }),
   ],
   module: {
@@ -27,12 +29,8 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: "asset/resource", // Handles assets such as images
       },
     ],
   },
